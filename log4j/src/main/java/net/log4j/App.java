f0076a7e6c936aa1f2d6ead01e5f4fe6f475eb2f
diligentdpf <1852300415@qq.com>
2017-12-07 09:30:03 +0800

    modify logback

--- log4j/src/main/java/net/log4j/App.java
+++ log4j/src/main/java/net/log4j/App.java
@@ -10,7 +10,7 @@ public class App
 	
     public static void main( String[] args )
     {
-    	logger.info("xxx");
-    	badWriter.info("xxx");
+    	logger.info("logger");
+    	badWriter.info("badWriter");
     }
 }
